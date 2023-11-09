e3a6c888f67224c460689d082de682ba21c28e68
ShaunFell <shaundbfell@gmail.com>
2023-11-09 19:41:39 +0100

    determined where the code was breaking down. A new off-shell definition of the auxiliary field is required. So far, simulation runs without fail
    Former-commit-id: 83e480e246541159808fc153c259a5583e77fef0

--- Examples/GeneralizedProca/ProcaField.impl.hpp
+++ Examples/GeneralizedProca/ProcaField.impl.hpp
@@ -161,11 +161,88 @@ void ProcaField<potential_t>::add_matter_rhs(
     m_potential.compute_potential(V, dVdA, dVddA, vars, gamma_UU);
 
 
+
+    //my evolution equations
+
+
+    //evolution equations for spatial part of vector field (index down)
+    FOR1(i){
+        total_rhs.Avec[i] = - vars.lapse*d1.phi[i] - vars.phi*d1.lapse[i] + advec.Avec[i];
+
+        FOR1(j){
+            total_rhs.Avec[i] += - vars.lapse*gamma_LL[i][j]*vars.Evec[j] + vars.Avec[j]*d1.shift[j][i];
+        };
+    };
+
+
+    //evolution equations for Electric vector field (index up)
+    FOR1(i){
+        total_rhs.Evec[i] = vars.lapse*vars.K*vars.Evec[i] + advec.Evec[i];
+
+        FOR1(j){
+            total_rhs.Evec[i] += vars.lapse*gamma_UU[i][j]*d1.Z[j] + 2*vars.lapse*dVdA*gamma_UU[i][j]*vars.Avec[j] - vars.Evec[j]*d1.shift[i][j];
+        }
+
+        FOR3(j,k,l){
+            total_rhs.Evec[i] += -d1.lapse[j] * gamma_UU[j][k]*gamma_UU[i][l]*(d1.Avec[l][k] - d1.Avec[k][l]) - vars.lapse*gamma_UU[j][k]*gamma_UU[i][l]*(d2.Avec[l][k][j] - d2.Avec[k][l][j]);
+
+            FOR1(m){
+                total_rhs.Evec[i] += -vars.lapse*gamma_UU[j][k]*gamma_UU[i][l]*(chris_phys[m][j][l]*(d1.Avec[k][m] - d1.Avec[m][k]) + chris_phys[m][j][k]*(d1.Avec[m][l] - d1.Avec[l][m]));
+            };
+        };
+    };
+
+
+    //evolution equation for auxiliary constraint-damping scalar field Z
+    total_rhs.Z = 2*vars.lapse*dVdA*vars.phi - m_params.vector_damping*vars.lapse*vars.Z + advec.Z;
+    FOR1(i){
+        total_rhs.Z += vars.lapse*d1.Evec[i][i];
+        FOR1(j){
+            total_rhs.Z += vars.lapse*chris_phys[i][i][j]*vars.Evec[j];
+        }
+    }
+
+    //covariant derivative of spatial part of Proca field
+    Tensor<2,data_t> DA;
+    FOR2(i,j){
+        DA[i][j] = d1.Avec[j][i];
+        FOR1(k){
+            DA[i][j] += chris_phys[k][i][j]*vars.Avec[k];
+        }
+    }
+
+    //Extrinsic curvature
+    Tensor<2, data_t> ExCurv;
+    FOR2(i,j){
+        ExCurv = (1./vars.chi)*(vars.A[i][j] + 1./3.*vars.h[i][j]*vars.K);
+    }
+
+    //evolution equation for the scalar part of the Proca field
+    data_t gnn { dVdA - 2.0*dVddA*vars.phi*vars.phi };
+    data_t mass { m_potential.m_params.mass };
+
+    total_rhs.phi = -vars.lapse*vars.Z*mass*mass/(2*gnn) + vars.lapse*dVdA*vars.phi*vars.K/(gnn) + advec.phi;
+    FOR1(i){
+        total_rhs.phi += 2*vars.lapse*dVddA*vars.phi*vars.Avec[i]*vars.Evec[i]/gnn;
+
+        FOR1(j){
+            total_rhs.phi += gamma_UU[i][j]*(-vars.lapse*dVdA/gnn*DA[i][j] - dVdA/gnn*vars.Avec[i]*d1.lapse[j] + 2*vars.lapse*dVddA/gnn*2*vars.phi*vars.Avec[i]*d1.phi[j]);
+
+            FOR2(k,l){
+                total_rhs.phi += gamma_UU[i][k]*gamma_UU[j][l]*(2*vars.lapse*dVddA/gnn*vars.phi*vars.Avec[i]*vars.Avec[j]*ExCurv[k][l] - 2*vars.lapse*dVddA/gnn*vars.Avec[i]*vars.Avec[j]*DA[k][l]);
+            }
+        }
+    }
+
+
+
+
 //Katy's evolution equations
-/*
+
     const double c4 = m_potential.m_params.self_interaction;
 
-    FOR1(i)
+
+/*     FOR1(i)
     {
         total_rhs.Avec[i] = -vars.lapse * d1.phi[i] -
                             vars.phi * d1.lapse[i] + advec.Avec[i];
@@ -175,7 +252,9 @@ void ProcaField<potential_t>::add_matter_rhs(
                 -vars.lapse * gamma_LL[i][j] * vars.Evec[j] +
                 vars.Avec[j] * d1.shift[j][i];
         }
-    }
+    }  */
+
+
 
     // variable for term (D_i A_j - D_j A_i)
     // NB Christoffel symbols cancel and take care with
@@ -184,14 +263,14 @@ void ProcaField<potential_t>::add_matter_rhs(
     FOR2(i, j) { diff_DA[i][j] = d1.Avec[j][i] - d1.Avec[i][j]; }
 
 
-    // Here we are defining often used terms
+/*     // Here we are defining often used terms
     // DA[i][j] = D_i A_j
     Tensor<2, data_t> DA;
     FOR2(i, j)
     {
         DA[i][j] = d1.Avec[j][i];
         FOR1(k) { DA[i][j] += -chris_phys[k][i][j] * vars.Avec[k]; }
-    }
+    } */
 
 
     // Xsquared = X^/mu X_/mu
@@ -200,7 +279,8 @@ void ProcaField<potential_t>::add_matter_rhs(
     FOR2(i, j) { Xsquared += gamma_UU[i][j] * vars.Avec[j] * vars.Avec[i]; }
 
     // NB This is for E^i with indices up
-    FOR1(i)
+
+/*     FOR1(i)
     {
         total_rhs.Evec[i] =
             vars.lapse * vars.K * vars.Evec[i] + advec.Evec[i];
@@ -228,12 +308,12 @@ void ProcaField<potential_t>::add_matter_rhs(
                                       chris_phys[m][k][j] * diff_DA[l][m]);
             }
         }
-    }
+    } */
 
-    // evolution equation for the damping term Z
+/*     // evolution equation for the damping term Z
     // dVdA = mu^2 ( 1 + 4 c4 A^k A_k - 12 c4 phi^2)
     // (ie the second part of the constraint, eqn 27)
-    total_rhs.Z =
+     total_rhs.Z =
         vars.lapse * (dVdA * vars.phi - m_params.vector_damping * vars.Z) +
         advec.Z;
 
@@ -245,15 +325,10 @@ void ProcaField<potential_t>::add_matter_rhs(
             total_rhs.Z +=
                 vars.lapse * chris_phys[i][i][j] * vars.Evec[j];
         }
-    }
+    } */
 
-    //Extrinsic curvature
-    Tensor<2, data_t> ExCurv;
-    FOR2(i,j){
-        ExCurv = (1./vars.chi)*(vars.A[i][j] + 1./3.*vars.h[i][j]*vars.K);
-    }
 
-    // DAScalar = D_i A^i
+/*     // DAScalar = D_i A^i
     data_t DA_scalar;
     DA_scalar = 0;
     FOR2(i, j) { DA_scalar += DA[i][j] * gamma_UU[i][j]; }
@@ -265,35 +340,36 @@ void ProcaField<potential_t>::add_matter_rhs(
         C += 4.0 * c4 * gamma_UU[i][j] * vars.Avec[j] * vars.Avec[i];
     }
 
-    total_rhs.phi = vars.lapse / C * (1.0 + 4.0 * c4 * Xsquared) *
+    total_rhs.phi = vars.lapse / gnn * dVdA *
                      (vars.K * vars.phi - DA_scalar)
                  // QUESTION: Should this be lapse * Z / C  or lapse * Z??
-                 - vars.lapse * vars.Z / C + advec.phi;
+                 //- vars.lapse * vars.Z / C + advec.phi;
+                 - vars.lapse * vars.Z / gnn + advec.phi;
     FOR1(i)
     {
-        total_rhs.phi += 8.0 * c4 * vars.phi * vars.lapse / C *
+        total_rhs.phi += 2 * dVddA * vars.lapse / gnn *
                     (vars.Evec[i] * vars.Avec[i]);
 
         FOR1(j)
         {
             total_rhs.phi +=
                 -gamma_UU[i][j] * vars.Avec[i] * d1.lapse[j] +
-                8.0 * c4 * vars.phi * vars.lapse / C *
+                2 * dVddA * vars.phi * vars.lapse / gnn *
                     (2.0 * vars.Avec[i] * d1.phi[j] * gamma_UU[i][j]);
 
             FOR2(k, l)
             {
                 total_rhs.phi +=
-                    -8.0 * c4 * vars.lapse / C * gamma_UU[i][k] *
+                    -2 * dVddA* vars.lapse / gnn * gamma_UU[i][k] *
                         gamma_UU[j][l] * vars.Avec[i] * vars.Avec[j] *
                         DA[k][l] +
-                    8.0 * c4 * vars.phi * vars.lapse / C *
+                    2 * dVddA * vars.phi * vars.lapse / gnn *
                         (-ExCurv[i][j] * vars.Avec[k] *
                             vars.Avec[l] * gamma_UU[i][k] * gamma_UU[j][l]);
             }
         }
-    }
-    */
+    }  */
+    
     
     //################################################################################
 #ifdef EQUATION_DEBUG_MODE
