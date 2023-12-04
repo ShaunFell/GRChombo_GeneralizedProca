c99e2ce31c90d52276452210277e9535460bb513
Francesco Gozzini <gozzini@thphys.uni-heidelberg.de>
2023-12-04 16:06:27 +0100

    Improved NanCheck from Katy
    Former-commit-id: 737f2ae416ec7cba2b79bd4cc90bdc765137bdc8

--- Source/BoxUtils/NanCheck.hpp
+++ Source/BoxUtils/NanCheck.hpp
@@ -7,55 +7,69 @@
 #define NANCHECK_HPP_
 
 #include "Cell.hpp"
-#include "Coordinates.hpp"
 #include "UserVariables.hpp"
-#include "GRAMRLevel.hpp"
-#include "BHAMR.hpp"
 
 /// This compute class checks for nans or very large values and aborts the
 /// execution when they appear displying a custom or default error message.
 class NanCheck
 {
   protected:
-
-    const BHAMR& m_bh_amr;
-    const double m_dx;
-    const std::array<double, CH_SPACEDIM> &m_center;
-    const std::string m_error_info;
-    const double m_max_abs;
+    const std::string m_error_info = "NanCheck";
+    const double m_max_abs = 1e20;
+    const double m_dx = 0.0;
 
   public:
+    NanCheck() {}
 
-    NanCheck(const BHAMR& amr,
-             const std::string a_error_info = "NanCheck",
-             const double a_max_abs = 1e20)
-        : NanCheck(amr, -1, {0.}, a_error_info, a_max_abs)
+    /// This constructor takes a string which will be displayed when nans happen
+    NanCheck(const std::string a_error_info) : m_error_info(a_error_info) {}
+
+    /// This allows us to output the physical coords if the dx value is passed
+    NanCheck(const double a_dx) : m_dx(a_dx) {}
+
+    /// This constructor takes a string which will be displayed when nans happen
+    /// as well as the grid spacing
+    NanCheck(const double a_dx, const std::string a_error_info)
+        : m_error_info(a_error_info), m_dx(a_dx)
     {
     }
 
-    NanCheck(const BHAMR& amr,
-             const double a_dx, const std::array<double, CH_SPACEDIM> &a_center,
-             const std::string a_error_info = "NanCheck",
-             const double a_max_abs = 1e20)
-        : m_bh_amr(amr), m_dx(a_dx), m_center(a_center), 
-          m_error_info(a_error_info), m_max_abs(a_max_abs)
+    // This constructor takes all arguments, note ordering to reduce potential
+    // to confuse dx and max_abs
+    NanCheck(const double a_dx, const std::string a_error_info,
+             const double a_max_abs)
+        : m_error_info(a_error_info), m_max_abs(a_max_abs), m_dx(a_dx)
     {
     }
 
     void compute(Cell<double> current_cell) const
     {
-        bool stop = false;
+        // stop is shared between all threads
+        bool stop;
+
+        // guard assignment to prevent a race
+        #pragma omp atomic write
+        stop = false;
+
         int num_vars = current_cell.get_num_in_vars();
         for (int ivar = 0; ivar < num_vars; ++ivar)
         {
             double val;
             current_cell.load_vars(val, ivar);
-            if (std::isnan(val) || abs(val) > m_max_abs)
+            if (std::isnan(val) || abs(val) > m_max_abs) {
+
+                // we want to exit if any of the threads find a nan
+                #pragma omp atomic write
                 stop = true;
+
+            }
+
         }
+
+        // This needs to be the master thread, otherwise some schedulers have trouble exiting
+        #pragma omp master
         if (stop)
         {
-#pragma omp single
             {
                 pout() << m_error_info
                        << "::Values have become nan. The current state is: \n";
@@ -64,17 +78,18 @@ class NanCheck
                     pout() << UserVariables::variable_names[ivar] << ": "
                            << current_cell.load_vars(ivar) << std::endl;
                 }
-                pout() << "Integer coordinates: " << current_cell.get_int_vect()
-                       << std::endl;
-                if (m_dx > 0)
+                IntVect iv = current_cell.get_int_vect();
+                pout() << "Integer coordinates: " << iv << std::endl;
+                if (m_dx != 0.0)
                 {
-                    Coordinates<double> coords{current_cell, m_dx, m_center};
-                    pout() << coords << std::endl;
+                    pout() << "with m_dx: " << m_dx << std::endl;
+                    RealVect position(iv + 0.5 * RealVect::Unit);
+                    position *= m_dx;
+                    pout() << "Physical coords relative to bottom left corner "
+                              "of domain: "
+                           << position << std::endl;
                 }
             }
-
-            // save a checkpoint before aborting
-            m_bh_amr.forceCheckpoint();
 
             MayDay::Error("Values have become nan.");
         }
