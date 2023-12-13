35be76e912bdfc2032dd95a3dda02a5579d17a1f
Francesco Gozzini <gozzini@thphys.uni-heidelberg.de>
2023-12-13 16:06:46 +0100

    Fixed 2 bugs (old lines 29 and 200). Reformat.
    Former-commit-id: e92498cff7011a99c12576c8d9214afa113dbee9

--- Examples/GeneralizedProca/ProcaField.impl.hpp
+++ Examples/GeneralizedProca/ProcaField.impl.hpp
@@ -4,221 +4,251 @@ implementation file for ProcaField.hpp
 
 #if !defined(PROCAFIELD_H_INCLUDED)
 #error "This file should only be included through ProcaField.hpp"
-#endif 
+#endif
 
 #ifndef PROCAFIELD_IMPL_H_INCLUDEDP
 #define PROCAFIELD_IMPL_H_INCLUDED
 
-//Remove these after debugging
+// Remove these after debugging
 #include "DebuggingTools.hpp"
-
 
 template <class potential_t>
 template <class data_t, template <typename> class vars_t>
 emtensor_t<data_t> ProcaField<potential_t>::compute_emtensor(
-        const vars_t<data_t> &vars, //the value of the variables
-        const vars_t<Tensor<1,data_t>> &d1, //the 1st derivatives
-        const Tensor<2, data_t> &h_UU, //the inverse metric
-        const Tensor<3, data_t> &chris_ULL //conformal christoffel symbols
-    ) const {
-        emtensor_t<data_t> out;
-
-        //compute contravariant physical spatial metric
-        Tensor<2,data_t> gamma_UU;
-        FOR2(i,j){
-            gamma_UU[i][j] = h_UU[i][j]/vars.chi;
-        };
-
-        //compute covariant physical spatial metric
-        Tensor<2, data_t> gamma_LL { TensorAlgebra::compute_inverse_sym(gamma_UU) }; //inverse of inverse metric -> pure covariant spatial metric
-
-        //compute physical christoffel symbols
-        Tensor<3, data_t> chris_phys_ULL {TensorAlgebra::compute_phys_chris(d1.chi, vars.chi, vars.h, h_UU, chris_ULL) };
-
-        //Compute potential and its derivatives
-        data_t V {0.}; //value of potential
-        data_t dVdA {0.}; //first derivative of Potential function w.r.t argument
-        data_t dVddA {0.}; //second derivative...
-        m_potential.compute_potential(V, dVdA, dVddA, vars, gamma_UU);
-        
-        // D_i A_j  3-covariant derivative of spatial covector
-        Tensor<2, data_t> DA;
-        FOR2(i,j){
-            DA[i][j] = d1.Avec[j][i];
-            FOR1(k){
-                DA[i][j] -= chris_phys_ULL[k][i][j]*vars.Avec[k];
-            };
-        };
-
-        //D_i A_j - D_j A_i
-        Tensor<2,data_t> DA_antisym;
-        FOR2(i,j){
-            DA_antisym[i][j] = d1.Avec[j][i] - d1.Avec[i][j];
-        };
-
-
-        //Electric Field Norm
-        data_t Enorm {0};
-        FOR2(i,j){
-            Enorm += gamma_LL[i][j]*vars.Evec[i]*vars.Evec[j];
-        };
-
-
-        /////Components of EM tensor
-
-        //Eulerian Energy 
-        out.rho = Enorm + dVdA*vars.phi*vars.phi + 1/2*V;
-        FOR4(i,j,k,l){
-            out.rho += gamma_UU[k][i]*gamma_UU[l][j]*DA[i][j]*DA_antisym[k][l];
-        };
-
-
-        //Eulerian Momentum
-        FOR1(i){
-            out.Si[i] = 0; //zero initialize
-            out.Si[i] += vars.phi*dVdA*vars.Avec[i];
-            
-            FOR1(j){
-                out.Si[i] += 1./2. * (vars.Evec[j]*DA_antisym[i][j]);
-            };
-        };
-
-        //Eulerian Stress
-        FOR2(i,j){
-            out.Sij[i][j] = 0; //zero initialize
-
-            out.Sij[i][j] += 1./2. * ( 2*dVdA*vars.Avec[i]*vars.Avec[j] - gamma_LL[i][j]*V + 2*gamma_LL[i][j]*Enorm);
-
-            FOR2(l,k){
-                out.Sij[i][j] += 1./2. * (-gamma_LL[i][l]*gamma_LL[j][k]*vars.Evec[l]*vars.Evec[k] + gamma_UU[k][l]*DA_antisym[i][l]*DA_antisym[j][k]);
-
-                FOR2(m,n){
-                    out.Sij[i][j] += -gamma_LL[i][j]*gamma_UU[m][l]*gamma_UU[n][k]*DA[m][n]*DA_antisym[l][k];
-                };
-            };
-        };
-
-        //Eulerian Stress scalar
-        out.S = 0.0;
-        FOR2(i,j){
-            out.S += out.Sij[i][j]*gamma_UU[i][j];
-        };
-
-        return out;
-    };
-
-
-template <class potential_t>
-template <class data_t, 
-            template <typename> class vars_t, 
-            template <typename> class diff2_vars_t, 
-            template <typename> class rhs_vars_t>
-void ProcaField<potential_t>::add_matter_rhs(
-        rhs_vars_t<data_t> &total_rhs, //RHS terms for all vars
-        const vars_t<data_t> &vars, //the value of the variables
-        const vars_t<Tensor<1, data_t>> &d1, //value of 1st derivs
-        const diff2_vars_t<Tensor<2, data_t>> &d2, //2nd derivs
-        const vars_t<data_t> &advec //value of the beta^i d_i(var) terms
-        ) const
+    const vars_t<data_t> &vars,          // the value of the variables
+    const vars_t<Tensor<1, data_t>> &d1, // the 1st derivatives
+    const Tensor<2, data_t> &h_UU,       // the inverse metric
+    const Tensor<3, data_t> &chris_ULL   // conformal christoffel symbols
+) const
 {
+    emtensor_t<data_t> out;
 
+    // compute contravariant physical spatial metric
+    Tensor<2, data_t> gamma_UU;
+    FOR2(i, j) { gamma_UU[i][j] = h_UU[i][j] * vars.chi; };
 
+    // compute covariant physical spatial metric
+    Tensor<2, data_t> gamma_LL{TensorAlgebra::compute_inverse_sym(
+        gamma_UU)}; // inverse of inverse metric -> pure covariant spatial
+                    // metric
 
-    //calculate conformal contravariant metric and conformal christoffel symbols
-    const Tensor<2,data_t> h_UU = TensorAlgebra::compute_inverse(vars.h);
-    const Tensor<3, data_t> chris_ULL = TensorAlgebra::compute_christoffel(d1.h, h_UU).ULL;
+    // compute physical christoffel symbols
+    Tensor<3, data_t> chris_phys_ULL{TensorAlgebra::compute_phys_chris(
+        d1.chi, vars.chi, vars.h, h_UU, chris_ULL)};
 
-    //compute physical christoffel symbols
-    Tensor<3, data_t> chris_phys {TensorAlgebra::compute_phys_chris(d1.chi, vars.chi, vars.h, h_UU, chris_ULL) };
-
-    //calulate physical contravariant spatial metric
-    Tensor<2, data_t> gamma_LL;
-    FOR2(i,j){
-        gamma_LL[i][j] = vars.h[i][j]/vars.chi;
-    }
-
-    //physical covariant spatial metric
-    Tensor<2, data_t> gamma_UU { TensorAlgebra::compute_inverse_sym(gamma_LL) };
-
-
-    //compute potential and its derivatives
-    data_t V {0.};
-    data_t dVdA {0.};
-    data_t dVddA {0.};
+    // Compute potential and its derivatives
+    data_t V{0.};     // value of potential
+    data_t dVdA{0.};  // first derivative of Potential function w.r.t argument
+    data_t dVddA{0.}; // second derivative...
     m_potential.compute_potential(V, dVdA, dVddA, vars, gamma_UU);
 
-
-    //evolution equations for spatial part of vector field (index down)
-    FOR1(i){
-        total_rhs.Avec[i] = - vars.lapse*d1.phi[i] - vars.phi*d1.lapse[i] + advec.Avec[i];
-
-        FOR1(j){
-            total_rhs.Avec[i] += - vars.lapse*gamma_LL[i][j]*vars.Evec[j] + vars.Avec[j]*d1.shift[j][i];
-        };
+    // D_i A_j  3-covariant derivative of spatial covector
+    Tensor<2, data_t> DA;
+    FOR2(i, j)
+    {
+        DA[i][j] = d1.Avec[j][i];
+        FOR1(k) { DA[i][j] -= chris_phys_ULL[k][i][j] * vars.Avec[k]; };
     };
 
+    // D_i A_j - D_j A_i
+    Tensor<2, data_t> DA_antisym;
+    FOR2(i, j) { DA_antisym[i][j] = d1.Avec[j][i] - d1.Avec[i][j]; };
 
-    //evolution equations for Electric vector field (index up)
-    FOR1(i){
-        total_rhs.Evec[i] = vars.lapse*vars.K*vars.Evec[i] + advec.Evec[i];
+    // Electric Field Norm
+    data_t Enorm{0};
+    FOR2(i, j) { Enorm += gamma_LL[i][j] * vars.Evec[i] * vars.Evec[j]; };
 
-        FOR1(j){
-            total_rhs.Evec[i] += vars.lapse*gamma_UU[i][j]*d1.Z[j] + 2*vars.lapse*dVdA*gamma_UU[i][j]*vars.Avec[j] - vars.Evec[j]*d1.shift[i][j];
-        }
+    /////Components of EM tensor
 
-        FOR3(j,k,l){
-            total_rhs.Evec[i] += -d1.lapse[j] * gamma_UU[j][k]*gamma_UU[i][l]*(d1.Avec[l][k] - d1.Avec[k][l]) - vars.lapse*gamma_UU[j][k]*gamma_UU[i][l]*(d2.Avec[l][k][j] - d2.Avec[k][l][j]);
+    // Eulerian Energy
+    out.rho = Enorm + dVdA * vars.phi * vars.phi + 1 / 2 * V;
+    FOR4(i, j, k, l)
+    {
+        out.rho +=
+            gamma_UU[k][i] * gamma_UU[l][j] * DA[i][j] * DA_antisym[k][l];
+    };
 
-            FOR1(m){
-                total_rhs.Evec[i] += -vars.lapse*gamma_UU[j][k]*gamma_UU[i][l]*(chris_phys[m][j][l]*(d1.Avec[k][m] - d1.Avec[m][k]) + chris_phys[m][j][k]*(d1.Avec[m][l] - d1.Avec[l][m]));
+    // Eulerian Momentum
+    FOR1(i)
+    {
+        out.Si[i] = 0; // zero initialize
+        out.Si[i] += vars.phi * dVdA * vars.Avec[i];
+
+        FOR1(j) { out.Si[i] += 1. / 2. * (vars.Evec[j] * DA_antisym[i][j]); };
+    };
+
+    // Eulerian Stress
+    FOR2(i, j)
+    {
+        out.Sij[i][j] = 0; // zero initialize
+
+        out.Sij[i][j] += 1. / 2. *
+                         (2 * dVdA * vars.Avec[i] * vars.Avec[j] -
+                          gamma_LL[i][j] * V + 2 * gamma_LL[i][j] * Enorm);
+
+        FOR2(l, k)
+        {
+            out.Sij[i][j] +=
+                1. / 2. *
+                (-gamma_LL[i][l] * gamma_LL[j][k] * vars.Evec[l] *
+                     vars.Evec[k] +
+                 gamma_UU[k][l] * DA_antisym[i][l] * DA_antisym[j][k]);
+
+            FOR2(m, n)
+            {
+                out.Sij[i][j] += -gamma_LL[i][j] * gamma_UU[m][l] *
+                                 gamma_UU[n][k] * DA[m][n] * DA_antisym[l][k];
             };
         };
     };
 
+    // Eulerian Stress scalar
+    out.S = 0.0;
+    FOR2(i, j) { out.S += out.Sij[i][j] * gamma_UU[i][j]; };
 
-    //evolution equation for auxiliary constraint-damping scalar field Z
-    total_rhs.Z = 2*vars.lapse*dVdA*vars.phi - m_params.vector_damping*vars.lapse*vars.Z + advec.Z;
-    FOR1(i){
-        total_rhs.Z += vars.lapse*d1.Evec[i][i];
-        FOR1(j){
-            total_rhs.Z += vars.lapse*chris_phys[i][i][j]*vars.Evec[j];
+    return out;
+};
+
+template <class potential_t>
+template <class data_t, template <typename> class vars_t,
+          template <typename> class diff2_vars_t,
+          template <typename> class rhs_vars_t>
+void ProcaField<potential_t>::add_matter_rhs(
+    rhs_vars_t<data_t> &total_rhs,             // RHS terms for all vars
+    const vars_t<data_t> &vars,                // the value of the variables
+    const vars_t<Tensor<1, data_t>> &d1,       // value of 1st derivs
+    const diff2_vars_t<Tensor<2, data_t>> &d2, // 2nd derivs
+    const vars_t<data_t> &advec // value of the beta^i d_i(var) terms
+) const
+{
+
+    // calculate conformal contravariant metric and conformal christoffel
+    // symbols
+    const Tensor<2, data_t> h_UU = TensorAlgebra::compute_inverse(vars.h);
+    const Tensor<3, data_t> chris_ULL =
+        TensorAlgebra::compute_christoffel(d1.h, h_UU).ULL;
+
+    // compute physical christoffel symbols
+    Tensor<3, data_t> chris_phys{TensorAlgebra::compute_phys_chris(
+        d1.chi, vars.chi, vars.h, h_UU, chris_ULL)};
+
+    // calulate physical contravariant spatial metric
+    Tensor<2, data_t> gamma_LL;
+    FOR2(i, j) { gamma_LL[i][j] = vars.h[i][j] / vars.chi; }
+
+    // physical covariant spatial metric
+    Tensor<2, data_t> gamma_UU{TensorAlgebra::compute_inverse_sym(gamma_LL)};
+
+    // compute potential and its derivatives
+    data_t V{0.};
+    data_t dVdA{0.};
+    data_t dVddA{0.};
+    m_potential.compute_potential(V, dVdA, dVddA, vars, gamma_UU);
+
+    // evolution equations for spatial part of vector field (index down)
+    FOR1(i)
+    {
+        total_rhs.Avec[i] =
+            -vars.lapse * d1.phi[i] - vars.phi * d1.lapse[i] + advec.Avec[i];
+
+        FOR1(j)
+        {
+            total_rhs.Avec[i] += -vars.lapse * gamma_LL[i][j] * vars.Evec[j] +
+                                 vars.Avec[j] * d1.shift[j][i];
+        };
+    };
+
+    // evolution equations for Electric vector field (index up)
+    FOR1(i)
+    {
+        total_rhs.Evec[i] = vars.lapse * vars.K * vars.Evec[i] + advec.Evec[i];
+
+        FOR1(j)
+        {
+            total_rhs.Evec[i] +=
+                vars.lapse * gamma_UU[i][j] * d1.Z[j] +
+                2 * vars.lapse * dVdA * gamma_UU[i][j] * vars.Avec[j] -
+                vars.Evec[j] * d1.shift[i][j];
+        }
+
+        FOR3(j, k, l)
+        {
+            total_rhs.Evec[i] += -d1.lapse[j] * gamma_UU[j][k] *
+                                     gamma_UU[i][l] *
+                                     (d1.Avec[l][k] - d1.Avec[k][l]) -
+                                 vars.lapse * gamma_UU[j][k] * gamma_UU[i][l] *
+                                     (d2.Avec[l][k][j] - d2.Avec[k][l][j]);
+
+            FOR1(m)
+            {
+                total_rhs.Evec[i] +=
+                    -vars.lapse * gamma_UU[j][k] * gamma_UU[i][l] *
+                    (chris_phys[m][j][l] * (d1.Avec[k][m] - d1.Avec[m][k]) +
+                     chris_phys[m][j][k] * (d1.Avec[m][l] - d1.Avec[l][m]));
+            };
+        };
+    };
+
+    // evolution equation for auxiliary constraint-damping scalar field Z
+    total_rhs.Z = 2 * vars.lapse * dVdA * vars.phi -
+                  m_params.vector_damping * vars.lapse * vars.Z + advec.Z;
+    FOR1(i)
+    {
+        total_rhs.Z += vars.lapse * d1.Evec[i][i];
+        FOR1(j)
+        {
+            total_rhs.Z += vars.lapse * chris_phys[i][i][j] * vars.Evec[j];
         }
     }
 
-    //covariant derivative of spatial part of Proca field
-    Tensor<2,data_t> DA;
-    FOR2(i,j){
+    // covariant derivative of spatial part of Proca field
+    Tensor<2, data_t> DA;
+    FOR2(i, j)
+    {
         DA[i][j] = d1.Avec[j][i];
-        FOR1(k){
-            DA[i][j] += chris_phys[k][i][j]*vars.Avec[k];
-        }
+        FOR1(k) { DA[i][j] += chris_phys[k][i][j] * vars.Avec[k]; }
     }
 
-    //Extrinsic curvature
+    // Extrinsic curvature
     Tensor<2, data_t> ExCurv;
-    FOR2(i,j){
-        ExCurv = (1./vars.chi)*(vars.A[i][j] + 1./3.*vars.h[i][j]*vars.K);
+    FOR2(i, j)
+    {
+        ExCurv[i][j] =
+            (1. / vars.chi) * (vars.A[i][j] + 1. / 3. * vars.h[i][j] * vars.K);
     }
 
-    //evolution equation for the scalar part of the Proca field
-    data_t gnn { dVdA - 2.0*dVddA*vars.phi*vars.phi };
-    data_t mass { m_potential.m_params.mass };
+    // evolution equation for the scalar part of the Proca field
+    data_t gnn{dVdA - 2.0 * dVddA * vars.phi * vars.phi};
+    data_t mass{m_potential.m_params.mass};
 
-    total_rhs.phi = -vars.lapse*vars.Z*mass*mass/(2*gnn) + vars.lapse*dVdA*vars.phi*vars.K/(gnn) + advec.phi;
-    FOR1(i){
-        total_rhs.phi += 2*vars.lapse*dVddA*vars.phi*vars.Avec[i]*vars.Evec[i]/gnn;
+    total_rhs.phi = -vars.lapse * vars.Z * mass * mass / (2 * gnn) +
+                    vars.lapse * dVdA * vars.phi * vars.K / (gnn) + advec.phi;
+    FOR1(i)
+    {
+        total_rhs.phi += 2 * vars.lapse * dVddA * vars.phi * vars.Avec[i] *
+                         vars.Evec[i] / gnn;
 
-        FOR1(j){
-            total_rhs.phi += gamma_UU[i][j]*(-vars.lapse*dVdA/gnn*DA[i][j] - dVdA/gnn*vars.Avec[i]*d1.lapse[j] + 2*vars.lapse*dVddA/gnn*2*vars.phi*vars.Avec[i]*d1.phi[j]);
+        FOR1(j)
+        {
+            total_rhs.phi +=
+                gamma_UU[i][j] * (-vars.lapse * dVdA / gnn * DA[i][j] -
+                                  dVdA / gnn * vars.Avec[i] * d1.lapse[j] +
+                                  2 * vars.lapse * dVddA / gnn * 2 * vars.phi *
+                                      vars.Avec[i] * d1.phi[j]);
 
-            FOR2(k,l){
-                total_rhs.phi += gamma_UU[i][k]*gamma_UU[j][l]*(2*vars.lapse*dVddA/gnn*vars.phi*vars.Avec[i]*vars.Avec[j]*ExCurv[k][l] - 2*vars.lapse*dVddA/gnn*vars.Avec[i]*vars.Avec[j]*DA[k][l]);
+            FOR2(k, l)
+            {
+                total_rhs.phi +=
+                    gamma_UU[i][k] * gamma_UU[j][l] *
+                    (2 * vars.lapse * dVddA / gnn * vars.phi * vars.Avec[i] *
+                         vars.Avec[j] * ExCurv[k][l] -
+                     2 * vars.lapse * dVddA / gnn * vars.Avec[i] *
+                         vars.Avec[j] * DA[k][l]);
             }
         }
     }
-    
-    
-    //################################################################################
+
+    // ################################################################################
 #ifdef EQUATION_DEBUG_MODE
     DEBUG_HEADER;
     DEBUG_OUT3(V, dVdA, dVddA);
@@ -232,21 +262,14 @@ void ProcaField<potential_t>::add_matter_rhs(
     DEBUG_OUT3(vars.Evec[0], vars.Evec[1], vars.Evec[2]);
     DEBUG_OUT4(gamma_UU[0][0], gamma_UU[0][1], gamma_UU[0][2], gamma_UU[1][1]);
     DEBUG_OUT2(gamma_UU[1][2], gamma_UU[2][2]);
-    FOR2(i,j){
+    FOR2(i, j)
+    {
         DEBUG_OUT(d1.Evec[i][j]);
-        FOR1(k){
-            DEBUG_OUT(chris_phys[i][j][k]);
-        }
+        FOR1(k) { DEBUG_OUT(chris_phys[i][j][k]); }
     }
     DEBUG_END;
-#endif //EQUATION_DEBUG_MODE
-    //################################################################################
-
-
+#endif // EQUATION_DEBUG_MODE
+    // ################################################################################
 }
 
-
-
-#endif //PROCAFIELD_IMPL_H_INCLUDED
-
-
+#endif // PROCAFIELD_IMPL_H_INCLUDED
