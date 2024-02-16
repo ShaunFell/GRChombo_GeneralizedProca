644f0714d200e88baf2e40219794fa3cd4d586e5
ShaunFell <shaundbfell@gmail.com>
2024-02-16 18:55:36 +0100

    added additional parameter checks
    Former-commit-id: f147b149444d4e3ea476682b6af84698fd78b998

--- Examples/GeneralizedProca/SimulationParameters.hpp
+++ Examples/GeneralizedProca/SimulationParameters.hpp
@@ -72,18 +72,29 @@ class SimulationParameters : public SimulationParametersBase
 
     void check_params()
     {
+        //parameter warnings
         warn_parameter("kerr_mass", kerr_params.mass, kerr_params.mass >= 0.0,
                        "should be >= 0.0");
 
         warn_parameter("inner_r", inner_r, inner_r != 0.0, "set to default parameters (0.0)");
         warn_parameter("outer_r", outer_r, outer_r != 200.0, "set to default parameter (200.0)");
 
+        warn_parameter("excision_with_AH", excise_with_AH, !excise_with_AH, "Using default value for excise_with_AH: false");
+        warn_parameter("excision_with_chi", excise_with_chi, !excise_with_chi, "Using default value for excise_with_chi: false");
+        warn_parameter("excision_with_cutoff", excise_with_cutoff, !excise_with_cutoff, "Using default value for excise_with_cutoff: false");
+
+        warn_parameter("excision_with_cutoff", excise_with_cutoff, !(excise_with_AH || excise_with_chi || excise_with_cutoff), "Not using any excision routine. Black hole interior will not be excised");
+       
+       
+        //parameter checks
+        check_parameter("excision_with_AH", excise_with_AH, !(excise_with_AH && !AH_activate), "AH Finder must be turned on to use dynamical excision");
+
+
         check_parameter("kerr_spin", kerr_params.spin,
                         std::abs(kerr_params.spin) <= kerr_params.mass,
                         "must satisfy |a| <= M = " +
                             std::to_string(kerr_params.mass));
 
-        check_parameter("excision_with_AH", excise_with_AH, !(excise_with_AH && !AH_activate), "AH Finder must be turned on to use dynamical excision");
 
         check_parameter("grid_scaling", grid_scaling, grid_scaling>0, "Grid scaling parameter must be greater than zero");
 
